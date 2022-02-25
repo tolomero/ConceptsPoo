a5cbbd01447dae088d397c818c17d7fabbd424f7
Javier Romero <tolomerog@gmail.com>
2022-02-25 16:19:24 -0500

    Finish Date class

---
+++ ConceptsPoo/ConceptsPoo/Date.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPoo
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            //si se le pone el underline ya no necesita el this
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month, day);
+        }
+
+   
+
+        //toca validar si es bisiesto el year
+        //en la linea 34 es donde validamos que el dia tiene q ser mayor igual a uno y que el dia se menor igual que los dias que debe tener el mes
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsLeepYear(year))
+            {
+                return day;
+            }
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+            throw new DayException("Invalid day");
+
+        } 
+
+        //toca comprobar si el year es modulo de 4 multiplo y es diferente de 100 es bisiesto y si es multiplo de 400 tambien
+        private bool IsLeepYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            //if (year % 4 == 0)
+            //{
+            //    if(year % 100 == 0)
+            //    {
+            //        if(year % 400 == 0)
+            //            {
+            //            return true;
+            //            }
+            //        else
+            //        {
+            //            return false;
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        return true;
+            //    }
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            throw new MonthException("Invalid month");
+        }
+
+        //metodo de sobre escritura con espacio me muestra todos lo metodos que son suseptibles a sobre escritura
+        public override string ToString()
+        {
+            return $"{_year}/{_month:00}/{_day:00}";
+        }
+    }
+}
---
+++ ConceptsPoo/ConceptsPoo/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConceptsPoo;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("==============");
+
+//Date? date1 = new Date(2022,2,25);
+
+//esto no sirve por que necesito una sobre escritura
+//Console.WriteLine(date1.ToString());
+//Console.WriteLine(date1);
+//para sacar el try catch que encierre lo que quiero es con control + ks luego escriba try
+try
+{
+    Console.WriteLine(new Date(1986, 5, 17));
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2000, 9, 30));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
